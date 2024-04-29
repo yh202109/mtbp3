49ea5603a99f41a49c2018886cf90f2d6f56b920
yh202109 <yh202109@gmail.com>
2024-04-29 13:09:34 -0400

    v0.2.2

--- mtbp3/health/clinical.py
+++ mtbp3/health/clinical.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2023 Y Hsu <yh202109@gmail.com>
+#  Copyright (C) 2023-2024 Y Hsu <yh202109@gmail.com>
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public license as published by
@@ -15,10 +15,31 @@
 
 import numpy as np
 import pandas as pd
-import random
 
 class summary_by_group:
+    """
+    A class for generating summary statistics and performing calculations on a DataFrame.
+
+    Attributes:
+        df (pandas.DataFrame): The input DataFrame.
+
+    Methods:
+        __init__(self, df): Initializes the summary_by_group class.
+        generate_dataset(design=1): Generate an example dataset for clinical health.
+        count_by_group(columns): Count occurrences of unique combinations of values in the DataFrame.
+        count_to_cate(group_columns, target_column, bins, stats): Convert numerical data into categorical data based on specified bins and calculate statistics.
+    """
+
     def __init__(self, df):
+        """
+        Initializes the summary_by_group class.
+
+        Args:
+            df (pandas.DataFrame): The input DataFrame.
+
+        Raises:
+            ValueError: If the input is not a DataFrame.
+        """
         if df is None:
             df = self.generate_dataset()
         self.df = df
@@ -29,7 +50,7 @@ class summary_by_group:
         Generate an example dataset for clinical health.
 
         Args:
-            type (int, optional): The type of dataset to generate. Defaults to 1.
+            design (int, optional): The type of dataset to generate. Defaults to 1.
 
         Returns:
             df (pandas.DataFrame): The generated example dataset.
@@ -59,6 +80,18 @@ class summary_by_group:
         return df
     
     def count_by_group(self, columns):
+        """
+        Count occurrences of unique combinations of values in the DataFrame.
+
+        Args:
+            columns (list): The columns to group by.
+
+        Returns:
+            grouped_df (pandas.DataFrame): The grouped DataFrame with count.
+
+        Raises:
+            ValueError: If the input is not a DataFrame or columns is not a list or a column does not exist in the DataFrame.
+        """
         if not isinstance(self.df, pd.DataFrame):
             raise ValueError("Input is not a DataFrame.")
         if not isinstance(columns, list):
@@ -70,6 +103,24 @@ class summary_by_group:
         return grouped_df
     
     def count_to_cate(self, group_columns, target_column, bins, stats):
+        """
+        Convert numerical data into categorical data based on specified bins and calculate statistics.
+
+        Args:
+            group_columns (list): The columns to group by.
+            target_column (str): The column to convert and calculate statistics on.
+            bins (int or list or numpy.ndarray): The bin edges for converting the target_column.
+            stats (list): The statistics to calculate.
+
+        Returns:
+            out1 (pandas.DataFrame): The resulting DataFrame with calculated statistics.
+
+        Raises:
+            ValueError: If the input is not a DataFrame or group_columns is not a list or contains non-string elements,
+                        target_column is not a non-empty string or does not exist in the DataFrame,
+                        target_column is not a numerical column, bins is not an integer or a sequence of bin edges,
+                        or stats is not a list of strings or contains invalid statistics.
+        """
         if not isinstance(self.df, pd.DataFrame):
             raise ValueError("Input is not a DataFrame.")
         if not isinstance(group_columns, list):
@@ -102,7 +153,7 @@ class summary_by_group:
                 out0.drop(columns=[target_column], inplace=True)
             elif stat == 'sum':
                 out0 = self.df.groupby(group_columns, observed=False)[target_column].apply(lambda x: np.histogram(x, bins=bins, weights=x)[0]).reset_index()
-                out0_columns = [f"Sum({bins[i]}<=X<{bins[i+1]})" for i in range(nb - 2)]+[f"S({bins[nb-2]}<=V<={bins[nb-1]})"]
+                out0_columns = [f"Sum({bins[i]}<=X<{bins[i+1]})" for i in range(nb - 2)]+[f"Sum({bins[nb-2]}<=V<={bins[nb-1]})"]
                 out0[out0_columns] = pd.DataFrame(out0[target_column].tolist(), index=out0.index)
                 out0.drop(columns=[target_column], inplace=True)
             elif stat == 'total_max':
