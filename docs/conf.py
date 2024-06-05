c83bd5198dae506b792eec63d11faee2481112f1
yh202109 <yh202109@gmail.com>
2024-06-05 12:46:01 -0400

    0.2.8

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx.ext.pngmath",
+    "sphinx.ext.imgmath",
 ]
 autoapi_dirs = ["../mtbp3"]
 
