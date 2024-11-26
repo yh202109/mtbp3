8c5f6bbc08967c49433777b482c54125823adc30
yh202109 <yh202109@gmail.com>
2024-11-26 13:40:40 -0500

    v0.2.23

--- mtbp3/seq/ictvmslview.py
+++ mtbp3/seq/ictvmslview.py
@@ -31,30 +31,34 @@ class ictvmsl:
 
         self.msl_version = ""
         self.msl_column_names = []
+        self.msl2 = pd.DataFrame()
         self.msl = self.__load_list()
 
         if self.msl is not None:
             self.msl_column_names = self.msl.columns.tolist()
             tmp = self.msl
             if 'Realm' in tmp.columns and 'Subrealm' in tmp.columns:
-                tmp['Realm'] = tmp.apply(lambda row: f"{row['Realm']}(sub-rank:{row['Subrealm']})" if pd.notna(row['Subrealm']) else row['Realm'], axis=1)
+                tmp['Realm'] = tmp.apply(lambda row: f"{row['Realm']}(Subrealm:{row['Subrealm']})" if pd.notna(row['Subrealm']) else row['Realm'], axis=1)
             if 'Kingdom' in tmp.columns and 'Subkingdom' in tmp.columns:
-                tmp['Kingdom'] = tmp.apply(lambda row: f"{row['Kingdom']}(sub-rank:{row['Subkingdom']})" if pd.notna(row['Subkingdom']) else row['Kingdom'], axis=1)
+                tmp['Kingdom'] = tmp.apply(lambda row: f"{row['Kingdom']}(suSubkingdom:{row['Subkingdom']})" if pd.notna(row['Subkingdom']) else row['Kingdom'], axis=1)
             if 'Phylum' in tmp.columns and 'Subphylum' in tmp.columns:
-                tmp['Phylum'] = tmp.apply(lambda row: f"{row['Phylum']}(sub-rank:{row['Subphylum']})" if pd.notna(row['Subphylum']) else row['Phylum'], axis=1)
+                tmp['Phylum'] = tmp.apply(lambda row: f"{row['Phylum']}(Subphylum:{row['Subphylum']})" if pd.notna(row['Subphylum']) else row['Phylum'], axis=1)
             if 'Class' in tmp.columns and 'Subclass' in tmp.columns:
-                tmp['Class'] = tmp.apply(lambda row: f"{row['Class']}(sub-rank:{row['Subclass']})" if pd.notna(row['Subclass']) else row['Class'], axis=1)
+                tmp['Class'] = tmp.apply(lambda row: f"{row['Class']}(Subclass:{row['Subclass']})" if pd.notna(row['Subclass']) else row['Class'], axis=1)
             if 'Order' in tmp.columns and 'Suborder' in tmp.columns:
-                tmp['Order'] = tmp.apply(lambda row: f"{row['Order']}(sub-rank:{row['Suborder']})" if pd.notna(row['Suborder']) else row['Order'], axis=1)
+                tmp['Order'] = tmp.apply(lambda row: f"{row['Order']}(Suborder:{row['Suborder']})" if pd.notna(row['Suborder']) else row['Order'], axis=1)
             if 'Family' in tmp.columns and 'Subfamily' in tmp.columns:
-                tmp['Family'] = tmp.apply(lambda row: f"{row['Family']}(sub-rank:{row['Subfamily']})" if pd.notna(row['Subfamily']) else row['Family'], axis=1)
+                tmp['Family'] = tmp.apply(lambda row: f"{row['Family']}(Subfamily:{row['Subfamily']})" if pd.notna(row['Subfamily']) else row['Family'], axis=1)
             if 'Genus' in tmp.columns and 'Subgenus' in tmp.columns:
-                tmp['Genus'] = tmp.apply(lambda row: f"{row['Genus']}(sub-rank:{row['Subgenus']})" if pd.notna(row['Subgenus']) else row['Genus'], axis=1)
+                tmp['Genus'] = tmp.apply(lambda row: f"{row['Genus']}(sSubgenus:{row['Subgenus']})" if pd.notna(row['Subgenus']) else row['Genus'], axis=1)
             tmp = tmp.iloc[:, :-3]
             self.msl2 = tmp.drop(columns=[col for col in tmp.columns if col.lower().startswith('sub')])
             print(f"File {self.msl_file_path} has been loaded")
             print("Column names:", self.msl.columns.tolist())
             print("Total number of rows:", len(self.msl))
+            realm_counts = self.msl['Realm'].value_counts()
+            print("Number of rows by 'Realm':")
+            print(realm_counts)
 
     def __load_list(self):
         #file_path = f'./mtbp3/data/supp_seq/ICTV_MSL39v4_example.csv'
@@ -65,7 +69,7 @@ class ictvmsl:
         else:
             raise FileNotFoundError(f"File not found: {file_path}")
 
-    def find_rows_given_str(self, search_str="", search_rank="Species", color="", narrow=False):
+    def find_rows_given_str(self, search_str="", search_rank="Species", color="", narrow=False, outfmt="simple", exact=False):
         """
         Find rows in the DataFrame that contain the given search string.
         Parameters:
@@ -89,22 +93,42 @@ class ictvmsl:
 
         if search_rank == "all":
             if narrow:
-                filtered_df = self.msl2[self.msl2.iloc[:, 1:9].apply(lambda row: search_str.lower() in row.astype(str).str.lower().values, axis=1)]
-                if color:
-                    for col in filtered_df[1:9]:
-                        filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color) if pd.notna(row) else row)
+                if exact:
+                    filtered_df = self.msl2[self.msl2.iloc[:, 1:9].apply(lambda row: search_str.lower() == row.astkkkype(str).str.lower().values, axis=1)]
+                    if color:
+                        for col in filtered_df[1:9]:
+                            filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color, exact=exact) if pd.notna(row) else row)
+                else:
+                    filtered_df = self.msl2[self.msl2.iloc[:, 1:9].apply(lambda row: search_str.lower() in row.astkkkype(str).str.lower().values, axis=1)]
+                    if color:
+                        for col in filtered_df[1:9]:
+                            filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color, exact=exact) if pd.notna(row) else row)
             else:
-                filtered_df = self.msl[self.msl.iloc[:, 1:16].apply(lambda row: search_str.lower() in row.astype(str).str.lower().values, axis=1)]
-                if color:
-                    for col in filtered_df[1:16]:
-                        filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color) if pd.notna(row) else row)
+                if exact:
+                    filtered_df = self.msl[self.msl.iloc[:, 1:16].apply(lambda row: search_str.lower() == row.astype(str).str.lower().values, axis=1)]
+                    if color:
+                        for col in filtered_df[1:16]:
+                            filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color, exact=exact) if pd.notna(row) else row)
+                else:
+                    filtered_df = self.msl[self.msl.iloc[:, 1:16].apply(lambda row: search_str.lower() in row.astype(str).str.lower().values, axis=1)]
+                    if color:
+                        for col in filtered_df[1:16]:
+                            filtered_df[col] = filtered_df[col].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color, exact=exact) if pd.notna(row) else row)
+
         else:
             if narrow:
-                filtered_df = self.msl2[self.msl2[search_rank].str.contains(search_str, case=False, na=False)]
+                if exact:
+                    filtered_df = self.msl2[self.msl2[search_rank].str.lower() == search_str.lower()]
+                else:
+                    filtered_df = self.msl2[self.msl2[search_rank].str.contains(search_str, case=False, na=False)]
             else:
-                filtered_df = self.msl[self.msl[search_rank].str.contains(search_str, case=False, na=False)]
+                if exact:
+                    filtered_df = self.msl2[self.msl2[search_rank].str.lower() == search_str.lower()]
+                else:
+                    filtered_df = self.msl[self.msl[search_rank].str.contains(search_str, case=False, na=False)]
+
             if color:
-                filtered_df[search_rank] = filtered_df[search_rank].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color))
+                filtered_df[search_rank] = filtered_df[search_rank].apply(lambda row: util.cdt.color_str(row, words=search_str, colors=color, exact=exact))
 
         return filtered_df
 
